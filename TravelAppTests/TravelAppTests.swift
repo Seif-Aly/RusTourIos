6229e462baeb7eb4b3959de8ac1d974733024839
seif <saly@edu.hse.ru>
2025-02-03 10:06:31 +0200

    Initial Commit

---
+++ TravelAppTests/TravelAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppTests.swift
+//  TravelAppTests
+//
+//  Created by seif on 03/02/2025.
+//
+
+import Testing
+@testable import TravelApp
+
+struct TravelAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
